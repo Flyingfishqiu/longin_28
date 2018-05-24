e8ec932f6d145ae1ebff31466f7e356fc0c23365
xwp <qiusijia@163.com>
2018-05-24 19:32:14 +0800

    封装代码

---
+++ config.py
@@ -0,0 +1,39 @@
+from redis import StrictRedis
+import logging
+class Config(object):
+
+    SECRET_KEY = 'q7pBNcWPgmF6BqB6b5VICF7z7pI+90o0O4CaJsFGjzRsYiya9SEgUDytXvzFsIaR'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI='mysql://root:mysql@127.0.0.1:3306/information_28'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = '6379'
+    SESSION_TYPE = 'redis'
+    SESSION_USE_SIGNER = True
+    # 创建redis连接对象
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    # 设置
+
+    PERMANENT_SESSION_LIFETIME = 60*60*1
+
+class DConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information_28'
+    CONFIG_LEVEL = logging.DEBUG
+
+class EConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information_29'
+    CONFIG_LEVEL = logging.ERROR
+
+class UConfig(Config):
+    TESTINg = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information_30'
+    CONFIG_LEVEL = logging.DEBUG
+
+
+deta_config = {
+    'd':DConfig,
+    'e':EConfig,
+    'u':UConfig
+}
--- menage.py
+++ menage.py
@@ -1,33 +1,8 @@
-from flask import  Flask
-from flask_sqlalchemy import SQLAlchemy
-from redis import StrictRedis
-from flask_wtf import CSRFProtect
 from flask import session
-from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
-# 配置信息
-class Config(object):
-    SECRET_KEY = 'q7pBNcWPgmF6BqB6b5VICF7z7pI+90o0O4CaJsFGjzRsYiya9SEgUDytXvzFsIaR'
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI='mysql://root:mysql@127.0.0.1:3306/information_28'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = '6379'
-    SESSION_TYPE = 'redis'
-    SESSION_USE_SIGNER = True
-    # 创建redis连接对象
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
-    # 设置
-
-    PERMANENT_SESSION_LIFETIME = 60*60*1
-app = Flask(__name__)
-
-# 加载配置信息
-app.config.from_object(Config)
-Session(app)
-CSRFProtect(app)
-db = SQLAlchemy(app)
+from info import con,db
+app = con('e')
 manage = Manager(app)
 Migrate(app,db)
 manage.add_command('mysql',MigrateCommand)
